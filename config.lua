07455d55bfcfcf8d8278de6c7313f8f268a1dc15
yzelast <yzelast@gmail.com>
2017-01-05 15:01:26 -0300

    basic config added

---
+++ config.lua
@@ -0,0 +1,12 @@
+-- Defines the base time of growth in seconds
+
+growth_time =	{
+				["apple"]  = 1,--450,
+				["pine"]   = 1, --330,
+				["aspen"]  = 1,--330,
+				["acacia"] = 1,--510,
+				["jungle"] = 1--570
+				}
+
+
+
